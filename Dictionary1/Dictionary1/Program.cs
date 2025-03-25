ca0609d3bf763e1f5af4dc8f1e907702a14ee7f8
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-03-25 17:47:00 +0200

    Add files via upload

---
+++ Dictionary1/Dictionary1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>()
+        {
+            { 1, "Иван" },
+            { 2, "Пешо" },
+            { 3, "Георги" },
+            { 4, "Елена" },
+            { 5, "Петър" },
+            { 6, "Виктория" }
+        };
+
+            Console.WriteLine("Въведете идентификационен номер на студент:");
+            int studentId = int.Parse(Console.ReadLine());
+
+            if (students.ContainsKey(studentId))
+            {
+                Console.WriteLine($"Студент с номер {studentId} е {students[studentId]}.");
+            }
+            else
+            {
+                Console.WriteLine("Няма такъв студент.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
