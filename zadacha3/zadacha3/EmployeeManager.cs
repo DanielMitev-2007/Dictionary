8a94fd735b53c76936ecf16be1e15a1146dd1cc8
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-03-30 11:33:54 +0300

    Add files via upload

---
+++ zadacha3/zadacha3/EmployeeManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha3
+{
+    internal class EmployeeManager
+    {
+        private SortedDictionary<int, string> employees = new SortedDictionary<int, string>();
+
+        public void AddEmployee(int id, string name)
+        {
+            if (employees.ContainsKey(id))
+            {
+                Console.WriteLine($"An employee with ID {id} already exists. The name will be updated.");
+                employees[id] = name;
+            }
+            else
+            {
+                employees.Add(id, name);
+            }
+        }
+
+        public void DisplayEmployees()
+        {
+            Console.WriteLine("\nList of employees, sorted by ID:");
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"ID: {employee.Key}, Name: {employee.Value}");
+            }
+        }
+    }
+}
---
+++ zadacha3/zadacha3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using zadacha3;
+
+class Program
+{
+    static void Main()
+    {
+        EmployeeManager employeeManager = new EmployeeManager();
+
+        while (true)
+        {
+            Console.WriteLine("Enter employee ID (or 'end' to stop):");
+            string idInput = Console.ReadLine();
+
+            if (idInput.ToLower() == "end")
+            {
+                break;
+            }
+
+            if (int.TryParse(idInput, out int id))
+            {
+                Console.WriteLine("Enter employee name:");
+                string name = Console.ReadLine();
+
+                employeeManager.AddEmployee(id, name);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid ID.");
+            }
+        }
+
+        employeeManager.DisplayEmployees();
+
+        Console.ReadKey();
+    }
+}
