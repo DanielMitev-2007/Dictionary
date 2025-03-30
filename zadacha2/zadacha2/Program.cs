96bb7d592c2c65401160cdc73b65f066e695ab25
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-03-30 11:27:14 +0300

    Add files via upload

---
+++ zadacha2/zadacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            WordCounter wordcounter = new WordCounter();
+            Console.WriteLine("Enter text: ");
+            string text = Console.ReadLine();
+            wordcounter.CountWords(text);
+            wordcounter.DisplayWordCount();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ zadacha2/zadacha2/WordCounter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha2
+{
+    internal class WordCounter
+    {
+        private Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+        public void CountWords(string text)
+        {
+            string[] words = text.Split(new char[] { ' ', '.', ',', '!', '?', ';', ':', '-', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                string lowerWord = word.ToLower();
+
+                if (wordFrequency.ContainsKey(lowerWord))
+                {
+                    wordFrequency[lowerWord]++;
+                }
+                else
+                {
+                    wordFrequency[lowerWord] = 1;
+                }
+            }
+        }
+
+        public void DisplayWordCount()
+        {
+            Console.WriteLine("\nFrequency of the word: ");
+
+            foreach (var entry in  wordFrequency)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+    }
+}
