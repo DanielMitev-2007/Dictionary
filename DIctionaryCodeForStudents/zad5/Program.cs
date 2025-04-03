4f864e308e94033b174f8381473f893de797b278
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-04-03 11:25:09 +0300

    Add files via upload

---
+++ DIctionaryCodeForStudents/zad5/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ComputerProgram> computerPrograms = ComputerProgramGenerator.GeneratePrograms();
+            PrintPrograms(computerPrograms);
+            Console.WriteLine();
+
+            if (ContainsDuplicate(computerPrograms))
+            {
+                Console.WriteLine("Virus is found!");
+            }
+            else
+            {
+                Console.WriteLine("Virus isn't found.");
+            }
+
+
+        }
+
+        public static bool ContainsDuplicate(List<ComputerProgram> programs)
+        {
+            Dictionary<int, int> idTracker = new Dictionary<int, int>();
+
+            foreach (var program in programs)
+            {
+                if (idTracker.ContainsKey(program.ID))
+                {
+                    return true;
+                }
+                idTracker[program.ID] = 1; 
+            }
+
+            return false;
+        }
+
+        public static void PrintPrograms(List<ComputerProgram> programs)
+        {
+            foreach (var program in programs)
+            {
+                Console.WriteLine(program.ToString());
+            }
+        }
+    }
+}
