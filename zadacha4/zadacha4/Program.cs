7470d95adbfa1e0048bb7b9910ab4da85035c4e3
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-04-03 11:17:35 +0300

    Add files via upload

---
+++ zadacha4/zadacha4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using zadacha4;
+class Program
+{
+            static void Main(string[] args)
+            {
+                TwoSumSolver solver = new TwoSumSolver();
+
+                Console.WriteLine("Enter numbers (separated with space):");
+                string[] input = Console.ReadLine().Split();
+                int[] nums = Array.ConvertAll(input, int.Parse);
+
+                Console.WriteLine("Enter target:");
+                int target = int.Parse(Console.ReadLine());
+
+                int[] result = solver.TwoSum(nums, target);
+
+                if (result[0] != -1)
+                {
+                    Console.WriteLine($"Result: Index: {result[0]} {result[1]}, numbers: {nums[result[0]]} {nums[result[1]]}");
+                }
+                else
+                {
+                    Console.WriteLine("There isn't that numbers.");
+                }
+            }
+        }
---
+++ zadacha4/zadacha4/TwoSumSolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha4
+{
+    internal class TwoSumSolver
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (dict.ContainsKey(complement))
+                {
+                    return new int[] { dict[complement], i }; 
+                }
+
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]] = i;
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
